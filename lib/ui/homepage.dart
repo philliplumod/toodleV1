635f4f017aff86870c3ce6cfe7225c4331352bd4
philliplumod <philliplumod@gmail.com>
2023-04-01 10:54:36 +0800

    add a change theme popup notification

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,5 +1,9 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:toddle/services/theme_services.dart';
+import 'package:toddle/ui/popup.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -30,6 +34,7 @@ class _HomePageState extends State<HomePage> {
       leading: GestureDetector(
         onTap: () {
           ThemeService().changeTheme();
+          PopService().showThemeChangeMessage();
         },
         child: const Icon(
           Icons.nightlight,
